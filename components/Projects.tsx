86d5816889f293e7b37f7b9d90846b15336dcfaa
vranasinghe <venujaranasinghe426@gmail.com>
2026-09-02 23:00:00 +0530

    update the project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -35,9 +35,7 @@ export default function Projects() {
         viewport={{ once: true }}
         className="mb-16 text-center"
       >
-        <p className="text-sm font-bold tracking-widest text-[var(--accent-color)] uppercase mb-3 drop-shadow-sm font-mono">
-          {/*Portfolio*/}
-        </p>
+
         <h2 className="text-4xl md:text-5xl font-extrabold text-[var(--text-primary)] mb-6 tracking-tight">
           Projects
         </h2>
